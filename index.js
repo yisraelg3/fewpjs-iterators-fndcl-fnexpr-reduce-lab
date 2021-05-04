7df03d26d05c71545da160773966b69e8922cbba
Yisrael Gurkow <yisrelg3@gmail.com>
2021-05-03 20:56:24 -0400

    commit tree ffd712d51d439c82af684e1e26dfc98300b4a91c

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+ const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+ 
+ const totalBatteries = batteryBatches.reduce((accumulator,number) => {
+     return number + accumulator
+ }, 0)
+ console.log(totalBatteries)
 
-// Code your solution here
+
+
+
+
+ // let battarray= []
+// // Code your solution here
+// const totalBatteries = batteryBatches.reduce((accumulation, number) => {
+//     accumulation.push(number)
+//     return accumulation
+// }, battarray)
+// const obj = {
+//     numbers:[]
+// }
+// const totalBatteries = batteryBatches.map((number) => {
+//     obj.numbers.push(number)
+
+// })
+
+//     console.log(obj)
